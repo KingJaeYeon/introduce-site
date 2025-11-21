352c0d3fbf6a056d4d637694a4b0e25556ee6be8
pyj <wodus331@gmail.com>
2025-11-21 21:38:15 +0900

    update: image Skeleton add

--- app/(introduce)/projects/components/ImgContentRenderer.tsx
+++ app/(introduce)/projects/components/ImgContentRenderer.tsx
@@ -3,6 +3,7 @@ import { useMediaQuery } from '@/hooks/useMediaQuery';
 import Image from 'next/image';
 import React, { useEffect, useState } from 'react';
 import { useModalStore } from '@/store/modalStore';
+import { Skeleton } from '@/components/ui/skeleton';
 
 export default function ImgContentRenderer({
   images,
@@ -14,6 +15,7 @@ export default function ImgContentRenderer({
   const isLg = useMediaQuery('(max-width: 1024px)');
   const { openModal } = useModalStore();
   const [mounted, setMounted] = useState(false);
+  const [loadedImages, setLoadedImages] = useState<Record<number, boolean>>({});
 
   useEffect(() => {
     setMounted(true);
@@ -22,17 +24,29 @@ export default function ImgContentRenderer({
   // 마운트 전에는 전체, 마운트 후에는 미디어쿼리 적용
   const displayImages = !mounted ? images : isLg ? images.slice(0, 5) : images;
 
+  const handleLoad = (index: number) => {
+    setLoadedImages((prev) => ({ ...prev, [index]: true }));
+  };
+
   return displayImages.map((item, index) => (
-    <Image
-      onClick={() => openModal('lightbox', { imgUrl: item })}
+    <div
       key={index}
-      src={item}
-      className={'h-[130px] w-auto border object-center hover:scale-105'}
-      alt={'alt'}
-      width={200}
-      height={130}
-      priority={isFrist} // 첫번째 이미지만 미리 로딩
-      loading={isFrist ? 'eager' : 'lazy'} // 나머지는 lazy
-    />
+      className="relative h-[130px] w-auto overflow-hidden rounded-md border"
+    >
+      {!loadedImages[index] && (
+        <Skeleton className="absolute inset-0 h-full w-full" />
+      )}
+      <Image
+        onClick={() => openModal('lightbox', { imgUrl: item })}
+        src={item}
+        alt={`image-${index}`}
+        className="h-[130px] w-auto object-center transition hover:scale-105"
+        width={200}
+        height={130}
+        priority={isFrist}
+        loading={isFrist ? 'eager' : 'lazy'}
+        onLoad={() => handleLoad(index)}
+      />
+    </div>
   ));
 }
